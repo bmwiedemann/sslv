23c5f046e6936de4a641c618fffe85d6e996638e
Bernhard M. Wiedemann <bwiedemann@suse.de>
2026-02-15 04:12:54 +0100

    Pretend gpg keys as not-expired

--- sslk/sslk.c
+++ sslk/sslk.c
@@ -58,3 +58,34 @@ CERT_VerifyCertificate(void)
 {
 	return (0);
 }
+
+/* Gnupg key expiry */
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <gpgme.h>
+#include <stdlib.h>
+#include <string.h>
+
+static gpgme_verify_result_t fake_result;
+
+gpgme_verify_result_t
+gpgme_op_verify_result(gpgme_ctx_t ctx)
+{
+    (void)ctx;
+
+    if (fake_result)
+        return fake_result;
+
+    fake_result = calloc(1, sizeof(*fake_result));
+    fake_result->signatures = calloc(1, sizeof(*fake_result->signatures));
+
+    gpgme_signature_t sig = fake_result->signatures;
+
+    sig->status   = GPG_ERR_NO_ERROR;
+    sig->validity = GPGME_VALIDITY_FULL;
+    sig->summary  = 0;
+
+    sig->fpr = strdup("AD485664E901B867051AB15F35A2F86E29B700A4");
+
+    return fake_result;
+}
